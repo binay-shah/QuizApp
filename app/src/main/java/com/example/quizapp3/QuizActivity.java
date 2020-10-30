3a6308c41bda520dd438a26f087010bdec62483f
binay-shah <binay18.shah@gmail.com>
2020-10-30 08:16:31 +0545

    initial commit

---
+++ app/src/main/java/com/example/quizapp3/QuizActivity.java
@@ -0,0 +1,29 @@
+package com.example.quizapp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QuizActivity extends AppCompatActivity {
+
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        submitButton = findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuizActivity.this, ScoreActivity.class);
+                intent.putExtra("SCORE", 5);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/quizapp3/ScoreActivity.java
@@ -0,0 +1,22 @@
+package com.example.quizapp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    private TextView scoreView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        Intent intent = getIntent();
+        int score = intent.getIntExtra("SCORE", 0);
+        scoreView.setText(""+score);
+    }
+}
